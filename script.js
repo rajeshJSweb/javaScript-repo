3d5ee77aa336698345f9e1b89cbfc50b961b911b
rajeshJS <rjshkhoksi26@gmail.com>
2021-07-03 14:08:29 +0600

    new change

---
+++ script.js
@@ -0,0 +1,146 @@
+/*var num1=10;
+var num2=15;
+var num3=7;
+var num4=12;
+var num5=9;
+
+if(num1 >= num2 && num1 >= num3 && num1 >= num4 && num1 > num5 ){
+    largest=num1;
+}
+else if(num2 => num1 && num2 > num3 && num2 > num4 && num2 > num5){
+    largest=num2;
+}
+else if(num3 => num1 && num3 > num2 && num3 > num4 && num3 > num5 ){
+    largest=num1;
+}
+else if(num4 => num1 && num4 > num2 && num4 > num3 && num4 > num5){
+    largest=num2;
+}
+else
+{
+    largest=num5;
+}
+console.log("Ths is the largest number=" +largest);
+
+var myName='Rajesh'
+
+if(myName==='Khoksi'){
+    console.log("You are allowed")
+}else{
+    console.log("You are not allowed")
+}
+
+
+var myAge=23;
+
+if(myAge < 18){
+    console.log('You are now adult')
+}
+else{
+    console.log('You are not adult')
+}
+
+
+
+var whatDate='Khoksi';
+
+if(whatDate === 'Saterday'){
+    console.log('This is reguar working day')
+}
+else if(whatDate === 'Sunday'){
+    console.log('This is also regular working days')
+}
+else if(whatDate === 'Monday'){
+    console.log('This is regular workng day')
+}
+else if(whatDate === 'Thurday'){
+    console.log('The day is regular working day')
+}
+else if(whatDate === 'Wedday'){
+    console.log('This is not holyday')
+}
+else if(whatDate === 'Khoksi'){
+    console.log('This is reguar half office day')
+}
+else if(whatDate === 'Friday'){
+    console.log('This is holyday')
+}
+else{
+    console.log('Nothing found this date list')
+}
+
+
+
+/*var rollNumber=prompt('Enter your roll number');
+
+switch(rollNumber){
+    case 45:
+        console.log('Rajesh Khoksi')
+        break;
+    case 46:
+        console.log('Hilary Areng')
+        break;    
+    case 50:
+        console.log('Thengsua Khoksi')
+        break;
+    default:
+        console.log('This roll number does not match in this list')        
+}
+*/
+
+/*
+
+var firstNumber=prompt('Enter your first number');
+var secondNumber=prompt('Enter your second number');
+var thirdNumber=prompt('Enter the third number')
+
+if(firstNumber >= secondNumber && firstNumber>= thirdNumber){
+    maxNumber=firstNumber;
+}
+else if(secondNumber >= firstNumber && secondNumber >= thirdNumber){
+    maxNumber=secondNumber;
+}
+else{
+    maxNumber=thirdNumber;
+}
+console.log('The number is maximum number=' +maxNumber);
+
+
+var myAge=prompt('Enter your age');
+
+if(myAge > 18){
+    if(myAge > 30){
+        console.log('You are a complete Man');
+    }
+    else if(myAge >= 60){
+        console.log('You are a Oldman');
+    }
+    else{
+        console.log('You are a Young Man');
+    }
+}
+else{
+    console.log('You are a Child');
+}
+*/
+
+
+var myTitle='khoksi'
+var myName='rajesh khoksi';
+var fullName;
+
+for(fullName='Hilary Areng'; fullName === myName; fullName++){
+    if(myTitle === 'khoksi'){
+    machong = myTitle;
+    }
+    else if(myName === "rajesh khoksi"){
+        machong = myName;
+    }
+
+    else{
+        machong = fullName;  
+    }
+    console.log(machong)
+}
+
+
